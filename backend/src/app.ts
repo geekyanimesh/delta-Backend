aa1f4a0adaaf6bcde36ec98121c5f820e4f5c1ee
geekyanimesh <animesh.kumar9651@gmail.com>
2025-07-27 17:31:21 +0530

    added vercel link to the cors file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,35 @@
 import express from 'express';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
 config();
 const app = express();
 
-app.use(cors({origin:"http://localhost:5173", credentials: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://delta-frontend-nu.vercel.app"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
 app.use(morgan("dev"));
 
-// middlewares
-app.use("/api/v1",appRouter);
+// Middlewares
+app.use("/api/v1", appRouter);
 
 export default app;
